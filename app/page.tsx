d7855bcff0511c765237335b44ecad8cc7d87660
LukeZHar <luke.z.harris@hotmail.com>
2025-03-21 23:59:31 +1000

    Chapter: 9

--- app/page.tsx
+++ app/page.tsx
@@ -8,8 +8,8 @@ import Image from 'next/image';
 
 export default function Page() {
   return (
-    <main className="flex min-h-screen flex-col p-6">
-      <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
+    <main className="flex min-h-screen flex-col p-6 ">
+      <div className="flex h-20 shrink-0 items-end rounded-lg bg-gradient-to-bl from-black via-emerald-900 to-black p-4 md:h-52">
         <AcmeLogo />
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
@@ -17,14 +17,14 @@ export default function Page() {
           <div className="relative w-0 h-0 border-l-[15px] border-r-[15px] border-b-[26px] border-l-transparent border-r-transparent border-b-black"></div>
           <p className={`${lusitana.className} antialiased`}>
             <strong>Welcome to Acme.</strong> This is the example for the{' '}
-            <a href="https://nextjs.org/learn/" className="text-blue-500">
+            <a href="https://nextjs.org/learn/" className="text-emerald-500">
               Next.js Learn Course
             </a>
             , brought to you by Vercel.
           </p>
           <Link
             href="/login"
-            className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
+            className="flex items-center gap-5 self-start rounded-lg bg-gradient-to-bl from-black via-emerald-900 to-black px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
           >
             <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
           </Link>
